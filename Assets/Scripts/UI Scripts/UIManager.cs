9460cf74e13b4758e5aa035bd583f10bf45da6ad
Wolfstreak25 <rathodroshan80@gmail.com>
2023-04-30 08:50:41 +0530

    added cinemamachine camera and modified levels

--- Assets/Scripts/UI Scripts/UIManager.cs
+++ Assets/Scripts/UI Scripts/UIManager.cs
@@ -6,14 +6,17 @@ public class UIManager : Singleton<UIManager>
 {
     [SerializeField]    private GameObject m_levelEndUI;
     [SerializeField]    private TextMeshProUGUI m_levelStatusText;
+    [SerializeField]    private GameObject m_nextLevelButton;
     public void LevelEndUI()
     {
         m_levelStatusText.text = $"{"Level Complete"}";
         m_levelEndUI.SetActive(true);
+        m_nextLevelButton.SetActive(true);
     }
     public void PlayerDeathUI()
     {
         m_levelStatusText.text = $"{"Player Died"}";
         m_levelEndUI.SetActive(true);
+        m_nextLevelButton.SetActive(false);
     }
 }
